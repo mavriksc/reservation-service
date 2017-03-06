db4dcc0ed9e0c388ec993ebf06513618b8ccc4b7
Sean Carlisle <s.m.carlisle@gmail.com>
2017-03-06 17:06:49 -0600

    messaging

--- reservation-service/src/main/java/com/example/ReservationServiceApplication.java
+++ reservation-service/src/main/java/com/example/ReservationServiceApplication.java
@@ -16,11 +16,13 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.cloud.stream.annotation.EnableBinding;
 import org.springframework.cloud.stream.annotation.Input;
+import org.springframework.cloud.stream.messaging.Sink;
 import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.data.rest.core.annotation.RestResource;
+import org.springframework.integration.annotation.IntegrationComponentScan;
 import org.springframework.integration.annotation.MessageEndpoint;
 import org.springframework.integration.annotation.ServiceActivator;
 import org.springframework.messaging.Message;
@@ -30,7 +32,8 @@ import org.springframework.web.bind.annotation.RestController;
 
 import lombok.Data;
 
-//@EnableBinding(ReservationChannels.class)
+@EnableBinding(Sink.class)
+@IntegrationComponentScan
 @EnableDiscoveryClient
 @SpringBootApplication
 public class ReservationServiceApplication {
@@ -48,25 +51,21 @@ public class ReservationServiceApplication {
 	}
 }
 
-//interface ReservationChannels {
-//    @Input
-//    MessageChannel input();
-//}
 
-//@MessageEndpoint
-//class ReservationProcessor {
-//    private final ReservationRepository reservationRepository;
-//    
-//    @ServiceActivator(inputChannel = "input")
-//    public void onMessage(Message<String> msg) {
-//        this.reservationRepository.save(new Reservation(msg.getPayload()));
-//        
-//    }
-//    @Autowired
-//    public ReservationProcessor(ReservationRepository reservationRepository){
-//        this.reservationRepository = reservationRepository;
-//    }
-//}
+@MessageEndpoint
+class ReservationProcessor {
+    private final ReservationRepository reservationRepository;
+    
+    @ServiceActivator(inputChannel = "input")
+    public void onMessage(Message<String> msg) {
+        this.reservationRepository.save(new Reservation(msg.getPayload()));
+        
+    }
+    @Autowired
+    public ReservationProcessor(ReservationRepository reservationRepository){
+        this.reservationRepository = reservationRepository;
+    }
+}
 
 @RefreshScope
 @RestController
